b45048ef4211fbb92cebb687be3a4051c3698564
Raj Patel <patel539@sheridancollege.ca>
2018-02-12 21:10:35 -0500

    Add multiple views including navigation functionality

---
+++ RajPatel_Assignment1/View Controllers/AboutAppViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutAppViewController.swift
+//  RajPatel_Assignment1
+//
+//  Created by Raj on 2018-02-12.
+//  Copyright © 2018 Raj. All rights reserved.
+//
+
+import UIKit
+
+class AboutAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
