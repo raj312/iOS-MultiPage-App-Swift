8765f389bc3341ef9d0258efff837701ad91bd1f
Raj Patel <patel539@sheridancollege.ca>
2018-02-16 21:08:43 -0500

    Add form control functionality

--- RajPatel_Assignment1/View Controllers/AboutYouViewController.swift
+++ RajPatel_Assignment1/View Controllers/AboutYouViewController.swift
@@ -8,8 +8,51 @@
 
 import UIKit
 
-class AboutYouViewController: UIViewController {
+class AboutYouViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var tfName: UITextField!
+    @IBOutlet var tfEmail: UITextField!
+    @IBOutlet var lbAge: UILabel!
+    @IBOutlet var lbGender: UILabel!
+    @IBOutlet var switchGender: UISwitch!
+    
+    //function to hide keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+    //Pop an alert to thank the user on submit click
+    @IBAction func formSubmitted(_ sender: UIButton) {
+        //need name and email from the information entered
+        let usName = tfName.text!
+        let usEmail = tfEmail.text!
+        
+        let showMessage: String = "Thank you \(usName) [\(usEmail)] for your time"
+        //making an alert object
+        let alert = UIAlertController(title: "Submission complete", message: showMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: .default, handler: { (action: UIAlertAction!) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        // present the alert
+        present(alert, animated: true)
+        
+    }
+    
+    // show slider value when the slider is changed
+    @IBAction func sliderValueChanged (sender: UISlider) {
+        let sliderVal = Int(sender.value)
+        lbAge.text = "\(sliderVal)"
+    }
+    
+    //function to change gender when the switch is changed
+    @IBAction func switchGenderChanged(sender: UISwitch) {
+        if sender.isOn {
+            lbGender.text = "Male"
+        }else {
+            lbGender.text = "Female"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
